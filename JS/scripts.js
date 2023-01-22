d65b9e7d4bfcd487f29d659d1a8d8c9562cd8881
adriviris <adrianacarrenog@gmail.com>
2023-01-22 17:07:36 -0500

    Fixed missing }

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,20 +1,16 @@
 
 function newItem(){
 
-//1. Adding a new item to the list of items: 
-
 let li = $('<li></li>');
 let inputValue = $('#input').val();
 li.append(inputValue);
 
-if(inputValue==='') {
+if (inputValue === '') {
     alert(" You must write something!");
-}else{
+} else {
     $('#list').append(li);
 }
-}
 
-//2. Crossing out an item from the list of items:
 function crossOut() {
     li.toggleClass("strike");
 }
@@ -23,8 +19,6 @@ li.on("dblclick", function crossOut() {
     li.toggleClass("strike");
 });
 
-
-//3(i). Adding the delete button "X": 
 let crossOutButton = $('<crossOutButton></crossOutButton');
 crossOutButton.append(document.createTextNode('X'));
 li.append(crossOutButton);
@@ -34,7 +28,4 @@ function deleteListItem(){
     li.addClass("delete")
 }
 $('#list').sortable();
-
-
-
-
+}
